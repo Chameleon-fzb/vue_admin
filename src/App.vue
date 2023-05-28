f72fd62e159ed808b858e511211ccd9026bd4197
fzb <1466518303@qq.com>
2023-05-29 00:24:23 +0800

    fitst commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const str = 'woainizhongguo'
+</script>
+<template>
+  <div class="box">
+    <h1>app根组件</h1>
+    <p>{{ str }}</p>
+  </div>
+</template>
+<style scoped lang="scss">
+.box {
+  width: 600px;
+  height: 600px;
+  background: black;
+  h1 {
+    color: white;
+  }
+}
+</style>
