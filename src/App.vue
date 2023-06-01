c8992966ded9d692ef28c994bf05f3f03420bdc2
fzb <1466518303@qq.com>
2023-06-01 15:44:56 +0800

    build:  @ replace './src' and feat: element-plus

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
+import HelloWorld from '@/components/HelloWorld.vue'
 const str = 'woainizhongguo'
 </script>
 <template>
   <div class="box">
     <h1>app根组件</h1>
     <p>{{ str }}</p>
+    <HelloWorld />
   </div>
 </template>
 <style scoped lang="scss">
