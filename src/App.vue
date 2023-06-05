d7e0efa2e59774abbd27607e24868f150476af96
fzb <1466518303@qq.com>
2023-06-05 17:15:20 +0800

    chore: add scss setting

--- src/App.vue
+++ src/App.vue
@@ -5,4 +5,12 @@ import SvgIcon from '@/components/SvgIcon/index.vue'
   <div class="box">
     <svg-icon width="30px" height="30px" name="copyright" color="red" />
   </div>
+  <div><h1>1111</h1></div>
 </template>
+<style lang="scss">
+div {
+  h1 {
+    color: red;
+  }
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
+//引入全局样式
+import '@/styles/index.scss'
 import 'element-plus/dist/index.css'
 import App from '@/App.vue'
 //svg插件需要的配置代码
 import 'virtual:svg-icons-register'
-//
+//自定义插件对象,注册整个项目全局组件
 import gloalComponent from '@/components'
 const app = createApp(App)
 app.use(gloalComponent)
